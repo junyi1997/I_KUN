2aacb720ad110a1c92fdaa6941ade04686982cf2
JohnnieWu <q5896799@gmail.com>
2019-09-12 14:10:59 +0800

    'first_commit'

---
+++ esp8266_wifi_connect/esp8266_wifi_connect.ino
@@ -0,0 +1,23 @@
+#include "ESP8266WiFi.h"
+ 
+const char* ssid = "IKUN";
+const char* password = "1234567890";
+ 
+void setup(void)
+{  
+  Serial.begin(9600);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {  //不斷嚐試連接
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected!");  //已連接
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());  //顯示IP位址
+}
+
+void loop() {
+
+}
