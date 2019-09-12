757808647cd01cf3920689ea15ee6560b84edd2e
JohnnieWu <q5896799@gmail.com>
2019-09-12 14:43:05 +0800

    'esp_wifi_connect_test'

--- esp8266_wifi_connect/esp8266_wifi_connect.ino
+++ esp8266_wifi_connect/esp8266_wifi_connect.ino
@@ -1,6 +1,6 @@
 #include "ESP8266WiFi.h"
  
-const char* ssid = "IFTTT";
+const char* ssid = "IKUN";
 const char* password = "1234567890";
  
 void setup(void)
@@ -16,8 +16,11 @@ void setup(void)
   Serial.println("WiFi connected!");  //已連接
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());  //顯示IP位址
+  pinMode(2,OUTPUT);
 }
 
 void loop() {
-
+  if (Serial.available()) {   //若軟體序列埠 Rx 收到資料 (來自 ESP8266)
+    Serial.write(Serial.read());  //讀取後寫入硬體序列埠 Tx (PC)
+  }
 }
